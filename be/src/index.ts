29903ea8a8c6ffe853276b06d26819c83c3db4bc
krish-sehgal <krishsehgal08@gmail.com>
2025-12-20 16:36:09 +0530

    add pre prompt

--- be/src/index.ts
+++ be/src/index.ts
@@ -19,7 +19,7 @@ export async function getGroqChatStream() {
     messages: [
       {
         role: "user",
-        content: "create a simple todo app",
+        content: "For all designs I ask you to make, have them be beautiful, not cookie cutter. Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI themes, icons, etc unless absolutely necessary or I request them.\n\nUse icons from lucide-react for logos.\n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.\n\ncreate a simple todo app",
       },
       {
         role: "system",
@@ -30,7 +30,7 @@ export async function getGroqChatStream() {
     // The language model which will generate the completion.
     model: "openai/gpt-oss-20b",
     temperature: 0.5,
-    max_completion_tokens: 2000,
+    max_completion_tokens: 1024,
 
     // Controls diversity via nucleus sampling: 0.5 means half of all
     // likelihood-weighted options are considered.
